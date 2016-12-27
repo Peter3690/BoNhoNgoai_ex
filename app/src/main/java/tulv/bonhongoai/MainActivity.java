b72748d2356cefb258684d1e3be123616f482a08
tuvl1990 <tulv1990@gmail.com>
2016-12-27 08:46:51 +0700

    BoNhoNgoai_ex 27/12/2016

---
+++ app/src/main/java/tulv/bonhongoai/MainActivity.java
@@ -0,0 +1,234 @@
+package tulv.bonhongoai;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends Activity {
+    private EditText editText;
+    private TextView textView;
+    private Button saveButton;
+    private Button readButton;
+    private Button listButton;
+
+
+    private static final int REQUEST_ID_READ_PERMISSION = 100;
+    private static final int REQUEST_ID_WRITE_PERMISSION = 200;
+
+    private final String fileName = "note.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+
+        saveButton = (Button) findViewById(R.id.button_save);
+        readButton = (Button) findViewById(R.id.button_read);
+        listButton = (Button) findViewById(R.id.button_list);
+
+        saveButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                askPermissionAndWriteFile();
+            }
+
+        });
+
+
+        readButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                askPermissionAndReadFile();
+            }
+
+        });
+
+        listButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                listExternalStorages();
+            }
+
+        });
+    }
+
+    private void askPermissionAndWriteFile() {
+        boolean canWrite = this.askPermission(REQUEST_ID_WRITE_PERMISSION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        //
+        if (canWrite) {
+            this.writeFile();
+        }
+    }
+
+    private void askPermissionAndReadFile() {
+        boolean canRead = this.askPermission(REQUEST_ID_READ_PERMISSION,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        //
+        if (canRead) {
+            this.readFile();
+        }
+    }
+
+
+    // Với Android Level >= 23 bạn phải hỏi người dùng cho phép các quyền với thiết bị
+    // (Chẳng hạn đọc/ghi dữ liệu vào thiết bị).
+    private boolean askPermission(int requestId, String permissionName) {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+
+            // Kiểm tra quyền
+            int permission = ActivityCompat.checkSelfPermission(this, permissionName);
+
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+
+                // Nếu không có quyền, cần nhắc người dùng cho phép.
+                this.requestPermissions(
+                        new String[]{permissionName},
+                        requestId
+                );
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Khi yêu cầu hỏi người dùng được trả về (Chấp nhận hoặc không chấp nhận).
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+
+        // Chú ý: Nếu yêu cầu bị hủy, mảng kết quả trả về là rỗng.
+        if (grantResults.length > 0) {
+            switch (requestCode) {
+                case REQUEST_ID_READ_PERMISSION: {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        readFile();
+                    }
+                }
+                case REQUEST_ID_WRITE_PERMISSION: {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        writeFile();
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Permission Cancelled!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void writeFile() {
+        // Thư mục gốc của SD Card.
+        File extStore = Environment.getExternalStorageDirectory();
+        // ==> /storage/emulated/0/note.txt
+        String path = extStore.getAbsolutePath() + "/" + fileName;
+        Log.i("ExternalStorageDemo", "Save to: " + path);
+
+        String data = editText.getText().toString();
+
+        try {
+            File myFile = new File(path);
+            myFile.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(myFile);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(data);
+            myOutWriter.close();
+            fOut.close();
+
+            Toast.makeText(getApplicationContext(), fileName + " saved", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readFile() {
+        // Thư mục gốc của SD Card.
+        File extStore = Environment.getExternalStorageDirectory();
+        // ==> /storage/emulated/0/note.txt
+        String path = extStore.getAbsolutePath() + "/" + fileName;
+        Log.i("ExternalStorageDemo", "Read file: " + path);
+
+        String s = "";
+        String fileContent = "";
+        try {
+            File myFile = new File(path);
+            FileInputStream fIn = new FileInputStream(myFile);
+            BufferedReader myReader = new BufferedReader(
+                    new InputStreamReader(fIn));
+
+            while ((s = myReader.readLine()) != null) {
+                fileContent += s + "\n";
+            }
+            myReader.close();
+
+            this.textView.setText(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(getApplicationContext(), fileContent, Toast.LENGTH_LONG).show();
+    }
+
+    private void listExternalStorages() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Data Directory: ").append("\n - ")
+                .append(Environment.getDataDirectory().toString()).append("\n");
+
+        sb.append("Download Cache Directory: ").append("\n - ")
+                .append(Environment.getDownloadCacheDirectory().toString()).append("\n");
+
+        sb.append("External Storage State: ").append("\n - ")
+                .append(Environment.getExternalStorageState().toString()).append("\n");
+
+        sb.append("External Storage Directory: ").append("\n - ")
+                .append(Environment.getExternalStorageDirectory().toString()).append("\n");
+
+        sb.append("Is External Storage Emulated?: ").append("\n - ")
+                .append(Environment.isExternalStorageEmulated()).append("\n");
+
+        sb.append("Is External Storage Removable?: ").append("\n - ")
+                .append(Environment.isExternalStorageRemovable()).append("\n");
+
+        sb.append("External Storage Public Directory (Music): ").append("\n - ")
+                .append(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).toString()).append("\n");
+
+        sb.append("Download Cache Directory: ").append("\n - ")
+                .append(Environment.getDownloadCacheDirectory().toString()).append("\n");
+
+        sb.append("Root Directory: ").append("\n - ")
+                .append(Environment.getRootDirectory().toString()).append("\n");
+
+        Log.i("ExternalStorageDemo", sb.toString());
+        this.textView.setText(sb.toString());
+    }
+
+}
